c4f9b14e01a2d72c20d457f72f069973206787f2
A3-Root <xmidnightsnowx@pm.me>
2024-10-18 21:13:35 +0530

    WIP

---
+++ addons/main/mod.cpp
@@ -0,0 +1,15 @@
+// https://community.bistudio.com/wiki/modParams
+name = "Root's Anomalies";
+author = "Root";
+// https://community.bistudio.com/wiki/ImageToPAA
+picture = "\x\zeus_additions\addons\main\ui\logo_zeus_additions.paa";
+logo = "\x\zeus_additions\addons\main\ui\logo_zeus_additions.paa";
+logoOver = "\x\zeus_additions\addons\main\ui\logo_zeus_additions.paa";
+logoSmall = "\x\zeus_additions\addons\main\ui\logo_zeus_additions.paa";
+tooltip = "Zeus Additions";
+tooltipOwned = "Zeus Additions";
+action = "https://github.com/johnb432/Zeus-Additions";
+actionName = "Github";
+overview = "A small mod that adds Zeus modules. Made by johnb43.";
+hidePicture = 0;
+hideName = 0;
---
+++ addons/main/script_mod.hpp
@@ -0,0 +1,8 @@
+#define MAINPREFIX z
+#define PREFIX root_anomalies
+
+#include "script_version.hpp"
+
+#define VERSION     MAJOR.MINOR
+#define VERSION_STR MAJOR.MINOR.PATCHLVL.BUILD
+#define VERSION_AR  MAJOR,MINOR,PATCHLVL,BUILD
